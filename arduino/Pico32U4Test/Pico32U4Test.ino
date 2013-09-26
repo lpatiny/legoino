dfb9ef242e1cfa351ed44a668642d84f087bf172
Luc Patiny <luc@patiny.com>
2013-09-26 21:35:42 +0200

    Update arduino

--- arduino/Pico32U4Test/Pico32U4Test.ino
+++ arduino/Pico32U4Test/Pico32U4Test.ino
@@ -5,22 +5,27 @@
  This example code is in the public domain.
  */
 
-#define OUT1    4
-#define OUT2    11
-#define OUT3    13
-#define OUT4    6
-#define IO1     8
-#define IO2     18
-#define IO3     20
-#define IO4     19
-#define RED1    5
-#define GREEN1  9
-#define BLUE1   10
-#define RED2    21
-#define GREEN2  22
-#define BLUE2   23
+#define OUT1        4
+#define OUT2        11
+#define OUT3        13
+#define OUT4        6
+#define IO1         8
+#define IO2         18
+#define IO3         20
+#define IO4         19
+#define RED1        5
+#define GREEN1      9
+#define BLUE1       10
+#define RED2        21
+#define GREEN2      22
+#define BLUE2       23
 
-#define TEST {OUT1,OUT2,OUT3,OUT4,IO1,IO2,IO3,IO4,RED1,GREEN1,BLUE1,RED2,GREEN2,BLUE2}
+#define SERIAL1_RX  2
+#define SERIAL1_TX  3
+#define I2C_SCL     0
+#define I2C_SDA     1
+
+#define TEST {OUT1,OUT2,OUT3,OUT4,IO1,IO2,IO3,IO4,RED1,GREEN1,BLUE1,RED2,GREEN2,BLUE2,SERIAL1_RX,SERIAL1_TX,I2C_SCL,I2C_SDA}
 
 byte test[]=TEST;
 void setup()  {                
